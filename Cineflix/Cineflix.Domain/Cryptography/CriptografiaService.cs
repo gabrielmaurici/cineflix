d4b4300d75c975424096b8fa08e34c26eb425d92
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-05-16 22:08:57 -0300

    Refatorado serviço de criptografia

--- Cineflix/Cineflix.Domain/Cryptography/CriptografiaService.cs
+++ Cineflix/Cineflix.Domain/Cryptography/CriptografiaService.cs
@@ -1,61 +1,43 @@
-﻿using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace Cineflix.Domain.Cryptography
 {
-    public  class CriptografiaService
+    public class CriptografiaService
     {
-        public string CriptografaSenha(string Senha)
+        public string CriptografaSenha(string senha)
         {
             using (MD5 md5Hash = MD5.Create())
             {
-                return GeraHash(md5Hash, Senha);
-            }
-        }
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(senha));
 
-        public bool VerificaSenha(string senhabanco, string Senha_MD5)
-        {
-            using (MD5 md5Hash = MD5.Create())
-            {
-                var senha = CriptografaSenha(senhabanco);
-                if (VerificarHash(md5Hash, Senha_MD5, senha))
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++)
                 {
-                    return true;
+                    sBuilder.Append(data[i].ToString("x2"));
                 }
-                else
-                {
-                    return false;
-                }
+
+                return sBuilder.ToString();
             }
         }
 
-        private string GeraHash(MD5 md5Hash, string input)
-        {
-            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+        //public bool VerificaSenha(string senhaGravada, string senhaInput)
+        //{
+        //    using (MD5 md5Hash = MD5.Create())
+        //    {
+        //        var senha = CriptografaSenha(senhaGravada);
+        //        StringComparer compara = StringComparer.OrdinalIgnoreCase;
 
-            StringBuilder sBuilder = new StringBuilder();
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                sBuilder.Append(data[i].ToString("x2"));
-            }
-
-            return sBuilder.ToString();
-        }
-
-        private bool VerificarHash(MD5 md5Hash, string input, string hash)
-        {
-            StringComparer compara = StringComparer.OrdinalIgnoreCase;
-
-            if (0 == compara.Compare(input, hash))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+        //        if (0 == compara.Compare(senha, senhaInput))
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //}
     }
 }
